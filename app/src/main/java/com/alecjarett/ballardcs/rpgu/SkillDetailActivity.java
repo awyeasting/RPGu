50fb112c6e2ce7da608850fde75f0fb1f58d31f6
Jarett Lund-Hopkins <jarettguy@gmail.com>
2016-06-06 20:06:39 -0700

    Expanded data on DetailActivity.

--- app/src/main/java/com/alecjarett/ballardcs/rpgu/SkillDetailActivity.java
+++ app/src/main/java/com/alecjarett/ballardcs/rpgu/SkillDetailActivity.java
@@ -2,17 +2,17 @@ package com.alecjarett.ballardcs.rpgu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+
 
 public class SkillDetailActivity extends AppCompatActivity {
+    public String skill;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.skill_detail_activity);
         Bundle extras = getIntent().getExtras();
-        String skill = extras.getString("Skill");
-        TextView skillView = (TextView) findViewById(R.id.skill_detail_text);
-        skillView.setText(skill);
+        skill = extras.getString("Skill");
     }
+
 
 
 }
