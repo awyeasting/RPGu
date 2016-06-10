023a1fced2734a5c242c10038ab77956b856e969
Alec Yeasting <awyeasting@gmail.com>
2016-06-10 08:22:09 -0700

    Implemented XP addition function and cleaned up activities in progress

--- app/src/main/java/com/alecjarett/ballardcs/rpgu/MainActivity.java
+++ app/src/main/java/com/alecjarett/ballardcs/rpgu/MainActivity.java
@@ -312,6 +312,7 @@ public class MainActivity extends AppCompatActivity {
             skills.add(new Skill("Wisdom", wisdomXP));
 
             skillsList = skills;
+            editor.commit();
         }
         return skillsList;
     }
@@ -332,11 +333,12 @@ public class MainActivity extends AppCompatActivity {
                         skillsList.add(new Skill(skillsList.get(i).getSkillLabel(), oldXP + xp));
                         skillsList.remove(i);
                         editor.putInt(skillName.toLowerCase() + "_xp", oldXP + xp);
+                        editor.commit();
                         return true;
                     }
                 }
             } else {
-                Log.e("hi,", "Invalid skillName input in addXPToSkill");
+                Log.e("hi,", "Invalid skillName input in addXPToSkill,"+skillName );
             }
         } catch (Exception e) {
             Log.e("hi,", "Error in addXPToSkill");
