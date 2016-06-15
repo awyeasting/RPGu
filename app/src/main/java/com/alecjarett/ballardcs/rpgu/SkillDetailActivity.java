7d8b0b0553e3ed80dfc69dff3f99f14b719c2cdc
Jarett Lund-Hopkins <jarettguy@gmail.com>
2016-06-14 18:04:38 -0700

    Made SkillDetailActivity pretty and hold more info

--- app/src/main/java/com/alecjarett/ballardcs/rpgu/SkillDetailActivity.java
+++ app/src/main/java/com/alecjarett/ballardcs/rpgu/SkillDetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
@@ -18,6 +19,12 @@ public class SkillDetailActivity extends AppCompatActivity {
         //Get skill data passed in to detail activity
         Bundle extras = getIntent().getExtras();
         Skill skill = (Skill)(extras.get("Skill"));
+
+        //Get some data about skill
+        int currentXP = skill.getXP();
+        int skillLevel = ExperienceFunctions.getLevel(currentXP);
+        int nextLevelXP = ExperienceFunctions.getExperience(skillLevel + 1);
+        int levelBaseXP = ExperienceFunctions.getExperience(skillLevel);
 
         //Set skill icon
         ImageView icon = (ImageView) findViewById(R.id.detail_page_icon);
@@ -36,16 +43,29 @@ public class SkillDetailActivity extends AppCompatActivity {
         skillName.setText(skill.getSkillLabel());
 
         //Set current skill level and level title
-        TextView currentLevelAndLevelTitle = (TextView) findViewById(R.id.detail_page_level_and_level_title);
-        currentLevelAndLevelTitle.setText("Level " + ExperienceFunctions.getLevel(skill.getXP()) + " " + ((MainActivity)getParent()).getLevelTitle(ExperienceFunctions.getLevel(skill.getXP())));
+        TextView currentLevelAndLevelTitle = (TextView) findViewById(R.id.skill_detail_level_text_view);
+        currentLevelAndLevelTitle.setText("Level " + skillLevel + " " + ((MainActivity)getParent()).getLevelTitle(skillLevel));
 
         //Set current xp earned
-        TextView xp = (TextView) findViewById(R.id.detail_page_xp);
-        xp.setText("XP: " + skill.getXP());
+        TextView xp = (TextView) findViewById(R.id.skill_detail_xp_text_view);
+        xp.setText("XP:\n" + currentXP);
 
         //Set xp needed until next level up
-        TextView xpToLevel = (TextView) findViewById(R.id.detail_page_xp_to_next_level);
-        xpToLevel.setText("XP To Next Level: " + ExperienceFunctions.getExperienceToNextLevel(skill.getXP()));
+        TextView xpToLevel = (TextView) findViewById(R.id.skill_detail_level_up_xp_text_view);
+        xpToLevel.setText("XP To Next Level:\n" + ExperienceFunctions.getExperienceToNextLevel(currentXP));
+
+        //Set up progress bar and textviews next to it
+        ProgressBar progressBar = (ProgressBar)(findViewById(R.id.skill_detail_progress_bar));
+
+        double levelProgress = ((double)(currentXP-levelBaseXP))/((double)(nextLevelXP-levelBaseXP));
+
+        progressBar.setProgress((int) (levelProgress * 100));
+
+        TextView currentLevel = (TextView) findViewById(R.id.skill_detail_current_level_progress);
+        currentLevel.setText(""+skillLevel);
+
+        TextView nextLevel = (TextView) findViewById(R.id.skill_detail_next_level_progress);
+        nextLevel.setText(""+skillLevel+1);
     }
 
 }
